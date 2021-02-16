bd3f6499b28deab4a913aa55261766b185c67c8b
ENRIQUE DIAZ DE LEON HICKS <enriquedlh97@hotmail.com>
2021-02-15 18:37:19 -0800

    First commit

---
+++ App01-MisDatos/App01_MisDatosApp.swift
@@ -0,0 +1,17 @@
+//
+//  App01_MisDatosApp.swift
+//  App01-MisDatos
+//
+//  Created by user189351 on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct App01_MisDatosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App01-MisDatos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App01-MisDatos
+//
+//  Created by Enrique Diaz de Leon Hicks on 2/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
